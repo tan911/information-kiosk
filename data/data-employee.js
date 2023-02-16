3eac5bb324e6d64e68a06a38034a1ab8ba2460a6
Jovan Lanutan <jolanutan@gmail.com>
2023-02-16 20:55:09 +0800

    added temporary data

---
+++ data/data-employee.js
@@ -0,0 +1,10 @@
+import Position from '../models/position';
+
+export const employeePosition = [
+  new Position('p1', 'Mayor Office', '#38bdf8'),
+  new Position('p2', 'Vice Mayor Office', '#38bdf8'),
+  new Position('p3', 'Vice Mayor Office', '#38bdf8'),
+  new Position('p4', 'Vice Mayor Office', '#38bdf8'),
+  new Position('p5', 'Vice Mayor Office', '#38bdf8'),
+  new Position('p6', 'Vice Mayor Office', '#38bdf8'),
+];
