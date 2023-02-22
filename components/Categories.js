7998303222a9f16b0d75f54ee7a565e2bd34baa0
Jovan Lanutan <jolanutan@gmail.com>
2023-02-22 20:25:15 +0800

    separated components categories

---
+++ components/Categories.js
@@ -0,0 +1,33 @@
+import { FlatList } from 'react-native';
+
+import { CATEGORIES } from '../data/data-employee';
+import PositionGridTile from '../components/PositionGridTile';
+
+function Categories({ navigation }) {
+  function renderEmployee(data) {
+    const presshandler = () => {
+      navigation.navigate('Departments', {
+        categoryID: data.item.id,
+      });
+    };
+
+    return (
+      <PositionGridTile
+        position={data.item.position}
+        color={data.item.color}
+        onPress={presshandler}
+      />
+    );
+  }
+
+  return (
+    <FlatList
+      key={'_'}
+      data={CATEGORIES}
+      keyExtractor={item => item.id}
+      renderItem={renderEmployee}
+      numColumns={1}
+    />
+  );
+}
+export default Categories;
