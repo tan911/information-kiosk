e97697d19fa3cd670dd4793b3248eaadfe4d6744
Jovan Lanutan <jolanutan@gmail.com>
2023-02-24 00:20:41 +0800

    dummy data

--- data/data-employee.js
+++ data/data-employee.js
@@ -2,7 +2,46 @@ import Branches from '../models/branches';
 import Departments from '../models/departments';
 
 export const CATEGORIES = [
-  new Branches('p1', 'EXECUTIVE'),
-  new Branches('p2', 'LEGISLATIVE'),
-  new Branches('p3', 'REMOTE OFFICES'),
+  new Branches('b1', 'EXECUTIVE'),
+  new Branches('b2', 'LEGISLATIVE'),
+  new Branches('b3', 'REMOTE OFFICES'),
+];
+
+export const DEPARTMENTS = [
+  new Departments(
+    'd1',
+    ['b1'],
+    'MICS Deparment',
+    'imageMAP',
+    'Engr. Jose R. Rizal',
+    '021312321313',
+    '5th floor'
+  ),
+  new Departments(
+    'd2',
+    ['b1'],
+    'HELLO Deparment',
+    'imageMAP',
+    'Engr. Jose R. Rizal',
+    '021312321313',
+    '5th floor'
+  ),
+  new Departments(
+    'd3',
+    ['b2'],
+    'Legislative Deparment',
+    'imageMAP',
+    'Engr. Jose R. Rizal',
+    '021312321313',
+    '5th floor'
+  ),
+  new Departments(
+    'd4',
+    ['b3'],
+    'remote Deparment',
+    'imageMAP',
+    'Engr. Jose R. Rizal',
+    '021312321313',
+    '5th floor'
+  ),
 ];
