8ac1a1b10891099cc7024e6649382a17820c2a34
Jovan Lanutan <jolanutan@gmail.com>
2023-02-24 00:15:54 +0800

    global colors

---
+++ utils/colors.js
@@ -0,0 +1,10 @@
+const Colors = {
+  primaryEx: '#86efac',
+  secondaryEx: '#4ade80',
+  primaryLeg: '#c4b5fd',
+  secondaryLeg: '#a78bfa',
+  primaryRem: '#fda4af',
+  secondaryRem: '#fb7185',
+};
+
+export default Colors;
