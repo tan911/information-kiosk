f90731b12aa9bfaf79c1fe31cede3c851266e893
Jovan Lanutan <jolanutan@gmail.com>
2023-02-24 00:16:41 +0800

    not-data departments

---
+++ components/DisplayedDepartment.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+function DisplayedDepartment({ title }) {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  );
+}
+export default DisplayedDepartment;
