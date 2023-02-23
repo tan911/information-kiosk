6c0e9a44c91a292ccdecf8e0019aaf2d923a5faa
Jovan Lanutan <jolanutan@gmail.com>
2023-02-24 00:18:19 +0800

    added background images

--- components/PositionGridTile.js
+++ components/PositionGridTile.js
@@ -1,14 +1,47 @@
-import { Pressable, View, Text, StyleSheet } from 'react-native';
+import {
+  Pressable,
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-function PositionGridTile({ position, onPress }) {
+import Colors from '../utils/colors';
+
+function PositionGridTile({ position, onPress, branch }) {
+  let primary, secondary, branchImage;
+
+  switch (branch) {
+    case 'EXECUTIVE':
+      (primary = Colors.primaryEx),
+        (secondary = Colors.secondaryEx),
+        (branchImage = '../assets/brachesImages/executive.png');
+      break;
+    case 'LEGISLATIVE':
+      (primary = Colors.primaryLeg), (secondary = Colors.secondaryLeg);
+      break;
+    case 'REMOTE OFFICES':
+      (primary = Colors.primaryRem), (secondary = Colors.secondaryRem);
+      break;
+    default:
+      (primary = ''), (secondary = ''), (branchImage = '');
+  }
+
   return (
-    <View style={styles.gridPosition}>
-      <Pressable style={styles.buttonBox} onPress={onPress}>
-        <View style={styles.innerContainer}>
-          <Text>{position}</Text>
-        </View>
-      </Pressable>
-    </View>
+    <LinearGradient style={styles.gridPosition} colors={[primary, secondary]}>
+      <ImageBackground
+        source={require('../assets/brachesImages/executive.png')}
+        resizeMode="stretch"
+        style={styles.innerContainer}
+        imageStyle={styles.backgroundImage}>
+        <Pressable style={styles.buttonBox} onPress={onPress}>
+          <View style={styles.innerContainer}>
+            <Text>{position}</Text>
+          </View>
+        </Pressable>
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
@@ -19,13 +52,13 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 16,
     height: 100,
-    backgroundColor: 'white',
     elevation: 4,
     shadowColor: 'black',
     shadowOpacity: 0.25,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
     borderRadius: 8,
+    overflow: 'hidden',
   },
   buttonBox: {
     flex: 1,
@@ -34,5 +67,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
