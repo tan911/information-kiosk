662ef4efb44a428ec373a5e1055b9ea54b157a94
Jovan Lanutan <jolanutan@gmail.com>
2023-02-24 13:10:08 +0800

    added image

--- models/branches.js
+++ models/branches.js
@@ -1,6 +1,9 @@
 class Branches {
-  constructor(id, position, color) {
-    (this.id = id), (this.position = position), (this.color = color);
+  constructor(id, position, color, image) {
+    (this.id = id),
+      (this.position = position),
+      (this.color = color),
+      (this.image = image);
   }
 }
 
