4e487c0263af94bfa3b237ad5d4d2b37997f4baf
Jovan Lanutan <jolanutan@gmail.com>
2023-02-24 13:07:21 +0800

    added splash screen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from './screens/HomeScreen';
 import DepartmentsOverviewScreen from './screens/DepartmentsOverviewScreens';
+import SplashScreen from './screens/SplashScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen
           name="Departments"
