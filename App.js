e363d04ed62095d1498312769117548f0a9c43e1
Jovan Lanutan <jolanutan@gmail.com>
2023-02-24 20:13:46 +0800

    added new screens

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import DepartmentsOverviewScreen from './screens/DepartmentsOverviewScreens';
 import SplashScreen from './screens/SplashScreen';
+import SearchScreen from './screens/SearchScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,7 @@ export default function App() {
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Departments" component={DepartmentsOverviewScreen} />
+        <Stack.Screen name="Search" component={SearchScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
