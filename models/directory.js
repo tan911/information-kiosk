384448d97f61bd8ded1dbb6f6ceac6757dfec7e7
Jovan Lanutan <jolanutan@gmail.com>
2023-02-25 13:10:17 +0800

    renamed: directory

---
+++ models/directory.js
@@ -0,0 +1,13 @@
+class Directory {
+  constructor(id, departmentIds, title, departmentHead, contactNumber, imageMap, location) {
+    this.id = id;
+    this.departmentIds = departmentIds;
+    this.title = title;
+    this.imageMap = imageMap;
+    this.departmentHead = departmentHead;
+    this.contactNumber = contactNumber;
+    this.location = location;
+  }
+}
+
+export default Directory;
